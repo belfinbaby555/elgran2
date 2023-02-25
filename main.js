13dd958df6f0467ad1bc18f50ce3e6c74b4a59a1
belfinbaby555 <105835749+belfinbaby555@users.noreply.github.com>
2023-02-25 19:15:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,20 +3,30 @@ function download(){
     document.getElementById("download").style.display="inherit";
 }
 function navi(){
-    if(document.getElementById("nav-item").style.height==="0%"){
-        document.getElementById("nav-item").style.height="40%"
+    var navi=document.getElementById("nav-item");
+    if(navi.style.height==="0%"){
+        navi.style.height="40%"
     }
     else{
-        document.getElementById("nav-item").style.height="0%";
+       navi.style.height="0%";
     }
 }
-function tabs(a){
-   if(a=="video"){
-    document.getElementById("audio").style.display="none";
-    document.getElementById("video").style.display="flex";
-   }
-   else{
-    document.getElementById("audio").style.display="flex";
-    document.getElementById("video").style.display="none";
-   }
-}
+
+function tabs(evt,tabName) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("videoandaudio");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("button2");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor="transparent";
+      tablinks[i].style.color="grey";
+    }
+  
+    document.getElementById(tabName).style.display = "block";
+    tablinks[evt].style.backgroundColor="rgb(255, 60, 0)";
+    tablinks[evt].style.color="white";
+  } 
